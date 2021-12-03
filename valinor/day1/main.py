8c3d79be49e121aab2bb5fd9808240e5c801b91d
Valinor <adrien.henriet@gmail.com>
2021-12-03 14:44:52 +0100

    Cosmetic correction from JCPlessis. Thank for the review

--- valinor/day1/main.py
+++ valinor/day1/main.py
@@ -1,7 +1,7 @@
 test_data = [int(i) for i in """199 200 208 210 200 207 240 269 260 263""".split()]
 
 def count_increase(data, sep=1):
-    return sum([data[i+sep] - data[i]>0 for i in range(0, len(data)-sep)])
+    return sum([data[i+sep] > data[i] for i in range(0, len(data)-sep)])
 
 if __name__ == '__main__':
     clean_data = [int(i) for i in open("input.txt").readlines()]
