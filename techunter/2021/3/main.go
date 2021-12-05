38f060b55c05090979c35a424eddd87713404c7c
TecHunter <a.baschenis@techunter.io>
2021-12-05 19:29:04 +0100

    day 3 - 2nd case

--- techunter/2021/3/main.go
+++ techunter/2021/3/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -74,25 +75,81 @@ func arrayToString(a []int, delim string) string {
 }
 
 func SecondCase(file string) int {
-	var lastSum = 0
-	var count = -1
-	// let's keep it simple :)
-	print("DAY-3 -- 2nd case -- input: ", file)
-	input, _ := ioutil.ReadFile(file)
-	arr := strings.Split(strings.ReplaceAll(string(input), "\r", ""), "\n")
-	for i := 0; i < len(arr)-2; i++ {
-		depthN, _ := strconv.Atoi(arr[i])
-		depthN1, _ := strconv.Atoi(arr[i+1])
-		depthN2, _ := strconv.Atoi(arr[i+2])
-		sum := depthN + depthN1 + depthN2
+	wd, _ := os.Getwd()
+	println("Workdir is ", wd)
+	println("DAY-3 -- 1st case -- input: ", file)
 
-		if sum > lastSum {
-			count++
-		}
-		lastSum = sum
+	input, _ := ioutil.ReadFile(file)
+	input = bytes.ReplaceAll(input, []byte("\r\n"), []byte("\n"))
+	var width = bytes.Index(input, []byte("\n"))
+	inputLength := len(input)
+	if input[inputLength-1] == byte('\n') {
+		inputLength--
 	}
-	println("\tResult: ", count)
-	return count
+	numberOfLines := (inputLength + 1) / (width + 1)
+	arr := make([][]byte, numberOfLines)
+	for i := 0; i < numberOfLines; i++ {
+		arr[i] = input[i*(width+1) : i*(width+1)+width]
+	}
+	println(numberOfLines, " lines of width ", width, ", total of ", inputLength, " bytes")
+	oxy := filter(arr, makeRange(0, numberOfLines), numberOfLines, 0, width, true)
+	co2 := filter(arr, makeRange(0, numberOfLines), numberOfLines, 0, width, false)
+
+	oxyRating, _ := strconv.ParseInt(string(arr[oxy]), 2, 32)
+	co2Rating, _ := strconv.ParseInt(string(arr[co2]), 2, 32)
+
+	result := oxyRating * co2Rating
+	println("\tResult: ", result)
+	return int(result)
+}
+
+func makeRange(min, max int) []int {
+	a := make([]int, max-min)
+	for i := range a {
+		a[i] = min + i
+	}
+	return a
+}
+
+func filter(input [][]byte, indexes []int, sizeOfIndexes int, offset int, width int, direction bool) int {
+	if sizeOfIndexes > 1 {
+		count0 := 0
+		count1 := 0
+		count0Indexes := make([]int, sizeOfIndexes)
+		count1Indexes := make([]int, sizeOfIndexes)
+		for i := 0; i < sizeOfIndexes; i++ {
+			lineIndex := indexes[i]
+			if input[lineIndex][offset] == byte('0') {
+				count0Indexes[count0] = lineIndex
+				count0++
+			} else {
+				count1Indexes[count1] = lineIndex
+				count1++
+			}
+		}
+		if count0 == count1 {
+			if direction {
+				return filter(input, count1Indexes, count1, offset+1, width, direction)
+			}
+			return filter(input, count0Indexes, count0, offset+1, width, direction)
+		}
+
+		if count0 > count1 {
+			if direction {
+				return filter(input, count0Indexes, count0, offset+1, width, direction)
+			}
+			return filter(input, count1Indexes, count1, offset+1, width, direction)
+
+		} else {
+			if direction {
+				return filter(input, count1Indexes, count1, offset+1, width, direction)
+			}
+			return filter(input, count0Indexes, count0, offset+1, width, direction)
+		}
+
+	}
+	return indexes[0]
+
 }
 
 /** The power consumption can then be found by multiplying the gamma rate by the epsilon rate. **/
