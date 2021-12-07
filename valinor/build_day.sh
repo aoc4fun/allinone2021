a74d90d01c68c4fbf17db65b0c9306621b64a6cf
Valinor <adrien.henriet@gmail.com>
2021-12-07 14:38:46 +0100

    Correct parameter

--- valinor/build_day.sh
+++ valinor/build_day.sh
@@ -1,1 +1,1 @@
-curl -s "https://adventofcode.com/2021/day/$1/input" -H "cookie: session=$(cat .sessionid)" -o day$day/input.txt
+curl -s "https://adventofcode.com/2021/day/$1/input" -H "cookie: session=$(cat .sessionid)" -o day$1/input.txt
